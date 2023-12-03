3c7949f16cda01922dddb1a93c66f7ac17fdc4f0
VictoriaAjala <victoria.o.ajala@gmail.com>
2023-12-03 13:09:04 +0100

    sneakylinkwinks

--- my_portfolio/src/components/NavBar.js
+++ my_portfolio/src/components/NavBar.js
@@ -2,10 +2,13 @@ import Link from "next/link";
 import React from "react";
 import Logo from "./Logo";
 import { useRouter } from "next/router";
+import { DribbbleIcon, GithubIcon, LinkedInIcon, MoonIcon, PinterestIcon, TwitterIcon } from "./Icons";
+import { motion } from "framer-motion";
 
 
 const CustomLink = ({href, title, className=""}) => {
     const router = useRouter();
+    
     return (
         <Link href={href} className={`${className} relative group`}>
             {title}
@@ -14,8 +17,8 @@ const CustomLink = ({href, title, className=""}) => {
             -bottom-0.5 group-hover:w-full transition-[width] ease duration-300
             ${router.aspath === href ? 'w-full' : 'w-0'}`}>&nbsp;</span>
         </Link>
-    )
-}
+    );
+};
 
 const NavBar = () => {
     return (
@@ -29,13 +32,43 @@ const NavBar = () => {
 
             </nav>
             
-            <nav>
-                <Link href="/" target={"_blank"}>T</Link>
-                <Link href="/" target={"_blank"}>T</Link>
-                <Link href="/" target={"_blank"}>T</Link>
-                <Link href="/" target={"_blank"}>T</Link>
-                <Link href="/" target={"_blank"}>T</Link>
-                <Link href="/" target={"_blank"}>T</Link>
+            <nav className="flex items-center justify-center flex-wrap">
+                <motion.a href="https://twitter.com/Vickyajala" target={"_blank"}
+                whileHover={{y:-2 }}
+                whileTap={{scale:0.9}}
+                className="w-6 mr-3">
+                    <TwitterIcon />
+                </motion.a>
+                <motion.a href="https://github.com/VictoriaAjala" target={"_blank"}
+                whileHover={{y:-2 }}
+                whileTap={{scale:0.9}}
+                className="w-6 mr-3">
+                    <GithubIcon />
+                </motion.a>
+                <motion.a href="https://www.linkedin.com/in/victoria-ajala/" target={"_blank"}
+                whileHover={{y:-2 }}
+                whileTap={{scale:0.9}}
+                className="w-6 mr-3">
+                    <LinkedInIcon />
+                </motion.a>
+                <motion.a href="/" target={"_blank"}
+                whileHover={{y:-2 }}
+                whileTap={{scale:0.9}}
+                className="w-6 mr-3">
+                    <DribbbleIcon />
+                </motion.a>
+                <motion.a href="/" target={"_blank"}
+                whileHover={{y:-2 }}
+                whileTap={{scale:0.9}}
+                className="w-6 mr-3">
+                    <PinterestIcon />
+                </motion.a>
+                <motion.a href="/" target={"_blank"}
+                whileHover={{y:-2 }}
+                whileTap={{scale:0.9}}
+                className="w-6 mr-3">
+                    <MoonIcon />
+                </motion.a>
 
             </nav>
 
@@ -43,7 +76,7 @@ const NavBar = () => {
             <Logo />
             </div>
         </header>
-    )
-}
+    );
+};
 
 export default NavBar
