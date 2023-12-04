8d0686c950220a085c6d1c88dc95b58d822e5787
VictoriaAjala <victoria.o.ajala@gmail.com>
2023-12-04 08:26:23 +0100

    margin

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -36,13 +36,13 @@ const NavBar = () => {
                 <motion.a href="https://twitter.com/Vickyajala" target={"_blank"}
                 whileHover={{y:-2 }}
                 whileTap={{scale:0.9}}
-                className="w-6 mr-3">
+                className="w-6 mx-3">
                     <TwitterIcon />
                 </motion.a>
                 <motion.a href="https://github.com/VictoriaAjala" target={"_blank"}
                 whileHover={{y:-2 }}
                 whileTap={{scale:0.9}}
-                className="w-6 mr-3">
+                className="w-6 mx-3">
                     <GithubIcon />
                 </motion.a>
                 <motion.a href="https://www.linkedin.com/in/victoria-ajala/" target={"_blank"}
@@ -54,19 +54,19 @@ const NavBar = () => {
                 <motion.a href="/" target={"_blank"}
                 whileHover={{y:-2 }}
                 whileTap={{scale:0.9}}
-                className="w-6 mr-3">
+                className="w-6 mx-3">
                     <DribbbleIcon />
                 </motion.a>
                 <motion.a href="/" target={"_blank"}
                 whileHover={{y:-2 }}
                 whileTap={{scale:0.9}}
-                className="w-6 mr-3">
+                className="w-6 mx-3">
                     <PinterestIcon />
                 </motion.a>
                 <motion.a href="/" target={"_blank"}
                 whileHover={{y:-2 }}
                 whileTap={{scale:0.9}}
-                className="w-6 mr-3">
+                className="w-6 ml-3">
                     <MoonIcon />
                 </motion.a>
 
