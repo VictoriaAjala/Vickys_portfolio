fd0c6d51569c44ae88a3e7ac6c5abc781e5d434e
VictoriaAjala <victoria.o.ajala@gmail.com>
2023-12-04 15:28:11 +0100

    contact

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import Layout from '@/components/Layout'
 import Head from 'next/head'
 import Image from 'next/image'
+import { LinkArrow } from '@/components/Icons';
 import ProfilePic from "../../public/images/profile/v3.png";
 import AnimatedText from '@/components/AnimatedText';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -18,12 +20,25 @@ export default function Home() {
             <div className='w-1/2'>
               <Image src={ProfilePic} alt="CodeBucks" className="w-full h-auto" />
             </div>
-            <div className='1/2'>
-              <AnimatedText text="Turning Vision Into Reality With Code And Design." className='!text-6xl' />
-              <p>
+            <div className='w-1/2 flex flex-col items-center self-center'>
+              <AnimatedText text="Turning Vision Into Reality With Code And Design." className='!text-6xl !text-left' />
+              <p className='my-4 text-base font-medium'>
               As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications. 
 Explore my latest projects and articles, showcasing my expertise in React.js and web development.
               </p>
+              <div className='flex items-center self-start mt-2'>
+                <Link href="/dummy.pdf" target={'_blank'}
+                className='flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg 
+                font-semibold hover:bg-light hover:text-dark border-2 border-solid 
+                border-transparent hover:border-dark'
+                download={true}
+                >Resume <LinkArrow className={"w-6 ml-1"} />
+
+                </Link>
+                <Link href="mailto:victoria.o.ajala@gmail.com" target={'_blank'}
+                className='ml-4 text-lg font-medium capitalize text-dark underline'
+                >Contact</Link>
+              </div>
             </div>
           </div>
         </Layout>
