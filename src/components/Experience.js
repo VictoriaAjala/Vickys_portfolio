e6f0267f35eb06e24aa5518d8dfada71a655cb59
VictoriaAjala <victoria.o.ajala@gmail.com>
2023-12-13 01:21:09 +0100

    added scrolleffect

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useRef } from "react";
+import { motion, useScroll } from "framer-motion";
 
 const Details = ({position, company, companyLink, time, address, work}) => {
     return <li className="my-8 first:mt-0 last:mb-0 w-[60% mx-auto flex flex-col items-center justify-between">
@@ -17,15 +18,26 @@ const Details = ({position, company, companyLink, time, address, work}) => {
 }
 
 const Experience = () => {
+    const ref = useRef(null);
+    const {scrollYProgress} = useScroll(
+        {
+            target: ref,
+            offset: ["start end", "center start"]
+        }
+    )
     return (
         <div className="my-64">
             <h2 className="font-bold text-8xl mb-32 w-full text-center">
             Experience
             </h2>
 
-            <div className="w-[75%] mx-auto relative ">
+            <div ref={ref} className="w-[45%] text-left mx-auto relative">
 
-                <ul>
+                <motion.div 
+                style={{scaleY: scrollYProgress}}
+                className="absolute left-[-50px] top-0 w-[4px] h-full bg-dark origin-top" />
+
+                <ul className="w-full flex flex-col items-start justify-between ml-12">
                    <Details 
                    position="Software Engineer" company="Google"
                    companyLink="www.google.com"
@@ -34,6 +46,25 @@ const Experience = () => {
                    search engine, including improving the accuracy and relevance of search results and 
                    developing new tools for data analysis and visualization."
                    />
+
+                    <Details 
+                   position="Software Engineer" company="Google"
+                   companyLink="www.google.com"
+                   time="2022-Present" address="Mountain View, CA"
+                   work="Worked on a team responsible for developing new features for Google's 
+                   search engine, including improving the accuracy and relevance of search results and 
+                   developing new tools for data analysis and visualization."
+                   />
+
+                    <Details 
+                   position="Software Engineer" company="Google"
+                   companyLink="www.google.com"
+                   time="2022-Present" address="Mountain View, CA"
+                   work="Worked on a team responsible for developing new features for Google's 
+                   search engine, including improving the accuracy and relevance of search results and 
+                   developing new tools for data analysis and visualization."
+                   />
+                   
                 </ul>
             </div>
 
